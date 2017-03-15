6cd82125787ee6b936b797a51ebe42de7c8cf205
Jonathan McCann <jonathan.mccann@liferay.com>
2017-03-15 13:09:09 -0700

    The strange formatting only occurs when the timer on the website is not counting down. If this is the case, do not update the title since it will never change.

--- timeLeft.js
+++ timeLeft.js
@@ -2,20 +2,11 @@ var timeLeftElement = document.getElementById("vi-cdown_timeLeft");
 var documentTitle = document.title;
 
 var getTimeLeft = function() {
-	var timeLeft;
-
-	var children = timeLeftElement.children;
-
-	if (children.length == 6) {
-		timeLeft = children[0].innerHTML + children[1].innerHTML + " " + children[2].innerHTML + children[3].innerHTML + " " + children[4].innerHTML + children[5].innerHTML;
-	}
-	else {
-		timeLeft = timeLeftElement.innerHTML;
-	}
+	var timeLeft = timeLeftElement.innerHTML;
 
 	document.title = timeLeft + " | " + documentTitle;
 }
 
-if (timeLeftElement !== null) {
+if ((timeLeftElement !== null) && (timeLeftElement.children.length == 0)) {
 	window.setInterval(getTimeLeft, 1000);
 }

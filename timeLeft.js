154126690b886072e83e39e2b54244a6fd101806
Jonathan McCann <jonathan.mccann@liferay.com>
2017-03-15 13:06:25 -0700

    Some auctions have strange formatting for their time left. Utilize the span's children to construct the correct title

--- timeLeft.js
+++ timeLeft.js
@@ -2,7 +2,16 @@ var timeLeftElement = document.getElementById("vi-cdown_timeLeft");
 var documentTitle = document.title;
 
 var getTimeLeft = function() {
-	var timeLeft = timeLeftElement.innerHTML;
+	var timeLeft;
+
+	var children = timeLeftElement.children;
+
+	if (children.length == 6) {
+		timeLeft = children[0].innerHTML + children[1].innerHTML + " " + children[2].innerHTML + children[3].innerHTML + " " + children[4].innerHTML + children[5].innerHTML;
+	}
+	else {
+		timeLeft = timeLeftElement.innerHTML;
+	}
 
 	document.title = timeLeft + " | " + documentTitle;
 }

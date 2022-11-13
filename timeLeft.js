9c78776669b52f24124dbf9ea2f543a817f3ae08
Jonathan McCann <jonathan.mccann@liferay.com>
2022-11-13 08:43:37 -0800

    Accommodate new count down timer

--- timeLeft.js
+++ timeLeft.js
@@ -1,12 +1,16 @@
-var timeLeftElement = document.getElementById("vi-cdown_timeLeft");
 var documentTitle = document.title;
+var timeLeftElement = document.getElementById("vi-cdown_timeLeft");
+var timerTextElements = document.getElementsByClassName('ux-timer__text');
 
-var getTimeLeft = function() {
-	var timeLeft = timeLeftElement.innerHTML;
+var getTimeLeft = function(element) {
+	var timeLeft = element.innerHTML;
 
 	document.title = timeLeft + " | " + documentTitle;
 }
 
 if (timeLeftElement !== null) {
-	window.setInterval(getTimeLeft, 1000);
+	window.setInterval(getTimeLeft, 1000, timeLeftElement);
+}
+else if (timerTextElements[0] !== undefined){
+	window.setInterval(getTimeLeft, 1000, timerTextElements[0]);
 }

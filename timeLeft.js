65c20d37cd194b8c9e9a64556b47fd19c09be1d5
Jonathan McCann <jonathan.mccann@liferay.com>
2023-08-20 15:00:41 -0700

    Remove unnecessary logic for obsolete timer

--- timeLeft.js
+++ timeLeft.js
@@ -1,14 +1,12 @@
 var documentTitle = document.title;
-var timeLeftElement = document.getElementById("vi-cdown_timeLeft");
 var timerTextElements = document.getElementsByClassName('ux-timer__text');
 
-var getTimeLeft = function(timeLeft) {
+var getTimeLeft = function(element) {
+	var timeLeft = element.innerText.replace("Ends in ", "");
+
 	document.title = timeLeft + " | " + documentTitle;
 }
 
-if (timeLeftElement !== null) {
-	window.setInterval(getTimeLeft, 1000, timeLeftElement.innerHTML);
-}
-else if (timerTextElements[0] !== undefined){
-	window.setInterval(getTimeLeft, 1000, timerTextElements[0].innerText.replace("Ends in ", ""));
+if (timerTextElements[0] !== undefined){
+	window.setInterval(getTimeLeft, 1000, timerTextElements[0]);
 }

e96dfc530262a3505ea833f86594c1390afc71a1
Jonathan McCann <jonathan.mccann@liferay.com>
2023-08-20 14:49:39 -0700

    Update for eBay UI changes

--- timeLeft.js
+++ timeLeft.js
@@ -2,15 +2,13 @@ var documentTitle = document.title;
 var timeLeftElement = document.getElementById("vi-cdown_timeLeft");
 var timerTextElements = document.getElementsByClassName('ux-timer__text');
 
-var getTimeLeft = function(element) {
-	var timeLeft = element.innerHTML;
-
+var getTimeLeft = function(timeLeft) {
 	document.title = timeLeft + " | " + documentTitle;
 }
 
 if (timeLeftElement !== null) {
-	window.setInterval(getTimeLeft, 1000, timeLeftElement);
+	window.setInterval(getTimeLeft, 1000, timeLeftElement.innerHTML);
 }
 else if (timerTextElements[0] !== undefined){
-	window.setInterval(getTimeLeft, 1000, timerTextElements[0]);
+	window.setInterval(getTimeLeft, 1000, timerTextElements[0].innerText);
 }
